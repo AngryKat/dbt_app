293ef10f726d5f90e303c4d068147739fd6454bf
AngryKat <gatto_candido@ukr.net>
2026-04-27 18:41:02 +0300

    feat: remove NoEmotionsFound component

--- src/screens/DistressEntries/features/Emotions/components/NoEmotionsFound/index.tsx
+++
@@ -1,9 +0,0 @@
-export function NoEmotionsFound({ searchTerm }: { searchTerm: string }) {
-  return (
-    <div className="grid h-full w-full place-items-center gap-2">
-      <p className="text-muted-foreground">
-        {`No emotions found for "${searchTerm}" entry :(`}
-      </p>
-    </div>
-  );
-}
